8b08b2e2b29cac0a8eb689478632dd0b8b626771
mwan0052 <mwan0052@student.monash.edu>
2021-10-17 20:13:31 +1100

    Charts Updated
    Fixed some bugs

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,20 @@
-var vg_1 = "chart_1.json";
-var vg_2 = "chart_2.json"
-vegaEmbed("#dot_plot", vg_1).then(function(result) {
+const vg_1 = "chart_1.json";
+const vg_2 = "chart_2.json";
+const vg_3 = "chart_3.json";
+const vg_4 = "chart_4.json";
+
+vegaEmbed("#chart1", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("#bar_chart", vg_2).then(function(result) {
+vegaEmbed("#chart2", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#chart3", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#chart4", vg_4).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
